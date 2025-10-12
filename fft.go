67fa00db0f0da14dccb88af04f349df38347e173
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-10-12 03:28:45 +0000

    Update CooleyTukey

--- fft.go
+++ fft.go
@@ -5,28 +5,78 @@ import (
 	"math/cmplx"
 )
 
-type FFT func(x []complex128) []complex128
-
 // CooleyTukey returns the discrete Fourier transform of the input sequence x
-func CooleyTukey(x []complex128) []complex128 {
+func CooleyTukey(x []complex128, inverse ...bool) []complex128 {
 	N := len(x)
-	if N < 2 {
-		return x
+	if N == 0 {
+		return nil
 	}
 
-	even, odd := make([]complex128, N/2), make([]complex128, N/2)
-	for i := range N / 2 {
-		even[i], odd[i] = x[2*i], x[2*i+1]
-	}
-	Feven, Fodd := CooleyTukey(even), CooleyTukey(odd)
-
+	// out
 	X := make([]complex128, N)
-	for k := range N / 2 {
-		a := -2i * math.Pi * complex(float64(k), 0) / complex(float64(N), 0)
-		t := cmplx.Exp(a) * Fodd[k]
+	copy(X, x)
 
-		X[k], X[k+N/2] = Feven[k]+t, Feven[k]-t
+	// bit-reverse
+	for i := range N {
+		j := reverse(i, N)
+		if j > i {
+			X[i], X[j] = X[j], X[i]
+		}
+	}
+
+	sign := -1.0
+	if len(inverse) > 0 && inverse[0] {
+		sign = 1.0
+	}
+
+	for s := 1; s < log2(N)+1; s++ {
+		m := 1 << s
+		m2 := m >> 1
+
+		a := complex(0, sign*2*math.Pi/float64(m))
+		wm := cmplx.Exp(a)
+
+		for k := 0; k < N; k += m {
+			w := complex(1, 0)
+			for j := range m2 {
+				t, u := w*X[k+j+m2], X[k+j]
+				X[k+j], X[k+j+m2] = u+t, u-t
+
+				w *= wm
+			}
+		}
+	}
+
+	if len(inverse) > 0 && inverse[0] {
+		for i := range N {
+			X[i] /= complex(float64(N), 0)
+		}
 	}
 
 	return X
+}
+
+func reverse(i, n int) int {
+	bits := uint(log2(n))
+
+	var result int
+	for b := range bits {
+		if (i>>b)&1 != 1 {
+			continue
+		}
+
+		result |= 1 << (bits - 1 - b)
+	}
+
+	return result
+}
+
+func log2(N int) int {
+	var count int
+	for N > 1 {
+		N >>= 1
+		count++
+	}
+
+	return count
 }
